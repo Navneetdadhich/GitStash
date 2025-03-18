00af9010d8e3e55e5c82fc2f2235bff63382bf7a
Nvntdad <nvntdadhich@gmail.com>
2025-03-18 23:30:05 +0530

    changed the url

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -26,7 +26,7 @@ const HomePage = () => {
 // console.log("GitHub API Key:", apiKey); // Debugging step
     setLoading(true);
   try {
-    const res = await fetch(`http://localhost:5000/api/users/profile/${username}`)
+    const res = await fetch(`https://gitstash.onrender.com/api/users/profile/${username}`)
     const {repos, userProfile} = await res.json();
     setRepos(repos);
     setUserProfile(userProfile);
